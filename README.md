a965d04df1892ef7a5bcefbd0156673c868b7cc9
Cyb3r G0d <77476823+CYB3R-G0D@users.noreply.github.com>
2021-06-21 20:38:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # SPY
-🕵️‍♂️ An OSINT python tool to scan social media accounts by username across social networks
+## _Social media scanning python tool_
+
+🕵️‍♂️ An OSINT tool to scan social media accounts by username across social networks
+
+## Installation
+
+clone or download the project
+
+```sh
+git clone https://github.com.cyb3r-g0d/SPY.git
+```
+
+## Usage
+
+Short Form    | Long Form     | Description
+------------- | ------------- |-------------
+-l            | --sitelist    | List of sites to scan (default=sites.txt)
+-t            | --num-threads | Number of threads to use for subbrute bruteforce
+-o            | --output      | Save the results to text file
+-h            | --help        | Show the help message
+
+### Examples
+
+* To list all the basic options and switches use -h switch:
+
+```python spy.py -h```
+
+* To find a  of specific username:
+
+``python spy.py alex``
+
+* To find a  of specific from custom sitelist:
+
+``python spy.py -l sitelixt.txt alex``
+
+
+### List Of Sites (210 Sites In Total!)
+View the list of sites [here](/res/site-names)
---
+++ spy.py
@@ -0,0 +1,82 @@
+# import modules urllib, thread, lock and queue  
+import urllib.request, urllib.error
+from threading import Thread, Lock
+from queue import Queue
+
+q = Queue()
+list_lock = Lock()
+discovered_usernames = []
+
+# scan_sites from username
+def scan_sites(username):
+    global q
+    while True:
+        sites = q.get()
+        url = f"{sites}{username}"
+        try:
+            conn = urllib.request.urlopen(url)
+        # 404 error code, page does not exist
+        except urllib.error.HTTPError as e:
+            # create text file and save profile pages that dosent exist
+            text_file = open(f"{username}""_not_found.txt", "a")
+            text_file.write(url)
+            text_file.write("\n")
+            text_file.close()
+            print('[!] Connection reffused to', f"{sites}")
+            print("[-] No user found:", f"{sites}")
+        # connection reffused to site    
+        except urllib.error.URLError as e:
+            # create text file error_logs.txt and enter sites that reffused to connect
+            text_file = open("error_logs/error_logs.txt", "a")
+            text_file.write(url)
+            text_file.write("\n")
+            text_file.close()
+            # show sites that reffused to connect
+            print('[!] Connection reffused to', f"{sites}")
+            pass
+        else:
+            # export found profile page urls to a text file
+            text_file = open(f"{username}"".txt", "a")
+            text_file.write(url)
+            text_file.write("\n")
+            text_file.close()  
+            print("[+] User found in", url)
+            with list_lock:
+                discovered_usernames.append(url)   
+        q.task_done()
+
+def main(username, n_threads, sites):
+    global q
+
+    for sites in sites:
+        q.put(sites)   
+
+    for t in range(n_threads):
+        worker = Thread(target=scan_sites, args=(username,))
+        worker.daemon = True
+        worker.start()
+
+
+if __name__ == "__main__":
+    import argparse
+    print("""
+ ___ _ __  _   _
+/ __| '_ \| | | |
+\__ | |_) | |_| |
+|___| .__/ \__, |
+    |_|    |___/
+# Social account scanner by @cyb3r-g0d
+    """ )      
+    parser = argparse.ArgumentParser(description="An OSINT tool to scan social media accounts by username across social networks")
+    parser.add_argument("username", help="username to scan for sites")
+    parser.add_argument("-l", "--sitelist", help="File that contains all sites to scan, line by line. Default is sites.txt",
+    default="res/sites.txt")
+    parser.add_argument("-t", "--num-threads", help="Number of threads to use to scan the username. Default is 10", default=10, type=int)
+
+    args = parser.parse_args()
+    username = args.username
+    sitelist = args.sitelist
+    num_threads = args.num_threads
+
+    main(username=username, n_threads=num_threads, sites=open(sitelist).read().splitlines())
+    q.join() 
